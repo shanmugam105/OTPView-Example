f8a817627c99dc858202f9dfc9d0062c66755dd1
Shanmugarajeshwaran R <tweets.shan@gmail.com>
2021-08-05 14:49:45 +0530

    Initial Commit

---
+++ OTPView-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OTPView-Example
+//
+//  Created by ADMIN on 05/08/21.
+//  Copyright © 2021 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
